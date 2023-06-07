c525632b50af6d1ae3a6ede6fde708094ce37ccf
surya-nand <83575893+surya-nand@users.noreply.github.com>
2023-06-07 23:31:32 +0530

    Initial  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Navbar,Cta,Brand} from './components';
+import { Blog, Header, WhatGPT3, Features, Possibility, Footer} from './containers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/containers/possibility/Posibility.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './possibility.css'
+
+export default function Posibility() {
+  return (
+    <div>Possibility</div>
+  )
+}
